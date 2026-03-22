97cd3fdf0a0afa1d991f14d49e91230419804a6a
KEV-AJ <porcher3842@gmail.com>
2026-03-22 11:38:19 +0100

    add private/jwt

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const service = require('../services/users');
+const private = require('../middlewares/private');
 
 
 router.get('/users', service.getAllUsers);
-router.get('/users/:email', service.getUserByEmail);
+router.get('/users/:email', private.checkJWT, service.getUserByEmail);
 router.post('/users', service.createUser);
-router.put('/users/:email', service.updateUserByEmail);
-router.delete('/users/:email', service.deleteUserByEmail);
+router.put('/users/:email', private.checkJWT, service.updateUserByEmail);
+router.delete('/users/:email', private.checkJWT, service.deleteUserByEmail);
 
 router.post('/users/authenticate', service.authenticate);
 
